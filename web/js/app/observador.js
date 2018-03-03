1370e156d03ebe60e2c68725b8af2aacb1ec4845
FiJus <fijusohalo@gmail.com>
2018-03-03 17:04:40 -0500

    ObservadorVisualizar

--- web/js/app/observador.js
+++ web/js/app/observador.js
@@ -1,47 +1,23 @@
 
 
- $('#SelectCurso').on('submit', function () {
-    var valor = $('#curso').val();
-    consultarEstudiantes(valor);
-    var casilla = $('#SelectEstudiante');
-    casilla.removeClass('hidden-sm-up');
-    var casilla2 = $('#estudiante');
-    casilla2.attr('required', false);
-    return false;
-});
+//$('#SelectCurso').on('submit', function () {
+//    var valor = $('#curso').val();
+//    consultarEstudiantes(valor);
+//    var casilla = $('#SelectEstudiante');
+//    casilla.removeClass('hidden-sm-up');
+//    var casilla2 = $('#estudiante');
+//    casilla2.attr('required', false);
+//    return false;
+//});
 
-$('#estudiante').on('change', function() {
+$('#curso').on('change', function () {
     $.ajax({
         type: 'GET',
         url: "ObservadorS",
         //force to handle it as text
         data: {
-            'opcion': "2",
-            'estudiante':  $('#estudiante').val()
-        },
-        dataType: "text",
-        success: function (data) {
-            var selectForm = $('#parrafo');
-            var json = $.parseJSON(data);
-            selectForm.empty();
-            for (var i = 0; i < json.length; ++i)
-            {
-                var opcion=json[i].detalles+","+json[i].calificacion;
-                selectForm.append(opcion);
-            }
-        },
-        async:false
-    });
-})
-
-function consultarEstudiantes(curso){
-    $.ajax({
-        type: 'GET',
-        url: "ObservadorS",
-        //force to handle it as text
-        data: {
-            'opcion' : "1",
-            'curso': curso
+            'opcion': "1",
+            'curso': $('#curso').val()
         },
         dataType: "text",
         success: function (data) {
@@ -52,11 +28,64 @@ function consultarEstudiantes(curso){
             console.log(json);
             for (var i = 0; i < json.length; ++i)
             {
-                var opcion="<option value=\""+json[i].idEstudiante+"\">"+json[i].nombre+"</option>"; 
+                var opcion = "<option value=\"" + json[i].idEstudiante + "\">" + json[i].nombre + "</option>";
+                selectForm.append(opcion);
+            }
+            var casilla = $('#SelectEstudiante');
+            casilla.removeClass('hidden-sm-up');
+            selectForm.attr('required', false);
+        },
+        async: false
+    });
+});
+
+$('#estudiante').on('change', function () {
+    $.ajax({
+        type: 'GET',
+        url: "ObservadorS",
+        //force to handle it as text
+        data: {
+            'opcion': "2",
+            'estudiante': $('#estudiante').val()
+        },
+        dataType: "text",
+        success: function (data) {
+            var selectForm = $('#parrafo');
+            var json = $.parseJSON(data);
+            selectForm.empty();
+            for (var i = 0; i < json.length; ++i)
+            {
+                var opcion = json[i].detalles + "," + json[i].calificacion;
                 selectForm.append(opcion);
             }
         },
-        async:false
+        async: false
     });
-    
-}
+});
+
+//function consultarEstudiantes(curso) {
+//    $.ajax({
+//        type: 'GET',
+//        url: "ObservadorS",
+//        //force to handle it as text
+//        data: {
+//            'opcion': "1",
+//            'curso': curso
+//        },
+//        dataType: "text",
+//        success: function (data) {
+//            var selectForm = $('#estudiante');
+//            selectForm.empty();
+//            selectForm.append('<option selected  value="" disabled>Seleccion un estudiante</option>');
+//            var json = $.parseJSON(data);
+//            console.log(json);
+//            for (var i = 0; i < json.length; ++i)
+//            {
+//                var opcion = "<option value=\"" + json[i].idEstudiante + "\">" + json[i].nombre + "</option>";
+//                selectForm.append(opcion);
+//            }
+//        },
+//        async: false
+//    });
+//
+//}
