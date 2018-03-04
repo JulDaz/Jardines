4dafdb3b526c6154b384a16ebb999038f4de3907
JulDa <JulDa@DESKTOP-5VLQ188>
2018-03-04 18:24:58 -0500

    Notas DAO y Servlet

---
+++ src/java/Dao/NotaDAO.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import Modelo.Nota;
+import Util.DbUtil;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author JulDa
+ */
+public class NotaDAO {
+
+    private Connection connection;
+
+    public NotaDAO() throws SQLException, URISyntaxException, ClassNotFoundException, IOException {
+        connection = DbUtil.getConnection();
+    }
+
+    public ArrayList<Nota> getNotaByIdEstudiante(int id_estudiante) throws SQLException, URISyntaxException {
+        ArrayList<Nota> notas = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("select * from nota where idestudiante=" + id_estudiante);
+        while (rs.next()) {
+            Nota n = new Nota();
+            n.setIdEstudiante(rs.getInt("id_Est"));
+            n.setNota(rs.getFloat("nota"));
+            notas.add(n);
+        }
+        return notas;
+    }
+    
+    public Nota getNotaByIdTema(int idTema) throws SQLException, URISyntaxException {
+        Nota nota= new Nota();
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("select * from nota where idTema=" + idTema);
+        while (rs.next()) {
+            nota.setNota(rs.getFloat("nota"));
+            nota.setIdEstudiante(rs.getInt("id_Est"));
+            nota.setIdTema(rs.getInt("id_Tema"));
+            nota.setDetallesExtra(rs.getString("detalles"));
+            
+        }
+        return nota;
+    }
+
+    public void addNota(Nota notica) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into nota values (?,?,?,?)");
+        preparedStatement.setFloat(1, notica.getNota());
+        preparedStatement.setInt(2, notica.getIdEstudiante());
+        preparedStatement.setInt(3, notica.getIdTema());
+        preparedStatement.setString(4, notica.getDetallesExtra());
+        preparedStatement.executeUpdate();
+    }
+}
